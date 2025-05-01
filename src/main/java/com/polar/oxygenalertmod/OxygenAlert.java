fd02db59995ea70bdfa200a7bb1e0b9bb0cc8a50
rafaelbraun2105 <rafaelbraun2105@gmail.com>
2025-05-01 20:25:12 +0000

    May this time work

--- src/main/java/com/polar/oxygenalertmod/OxygenAlert.java
+++ src/main/java/com/polar/oxygenalertmod/OxygenAlert.java
@@ -1,5 +1,6 @@
 package com.polar.oxygenalertmod;
 
+import net.minecraft.client.resources.sounds.SimpleSoundInstance;
 import net.minecraft.tags.FluidTags;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
@@ -8,6 +9,8 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.sounds.SoundEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
@@ -35,14 +38,37 @@ public class OxygenAlert {
         LocalPlayer player = mc.player;
        
         if (player == null) return;
-      
+
+        ModMain.LOGGER.info("Current air supply: {}", air);
         int air = player.getAirSupply();
         
         if (!player.isEyeInFluid(FluidTags.WATER)) {
             already_played_30 = false;
             already_played_10 = false;
+            ModMain.LOGGER.info("Not in water, resetting flags");
             return;
         }
+        if (air <= 30 && !already_played_30){
+            ModMain.LOGGER.info("Playing oxygen_alert_30 sound");
+            playAlertSound(ModRegistry.OXYGEN_ALERT_30.get());
+            already_played_30=true;
+        }
+        if (air <= 10 && !already_played_10){
+            ModMain.LOGGER.info("Playing oxygen_alert_10 sound");
+            playAlertSound(ModRegistry.OXYGEN_ALERT_10.get());
+            already_played_10 =true;
+        }
+    }
+
+    private void playAlertSound(SoundEvent soundEvent) {
+        Minecraft mc = Minecraft.getInstance();
+        LocalPlayer player = mc.player;
+        if (player != null && mc.getSoundManager() != null) {
+            SimpleSoundInstance sound = SimpleSoundInstance.forPlayer(soundEvent, player.getSoundSource(), player.getX(), player.getY(), player.getZ());
+            mc.getSoundManager().play(sound);
+
+        }
+    }
 
         
 
